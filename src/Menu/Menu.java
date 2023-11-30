a628c77a201b62435168e860e966a0ec281b7056
gnuh24 <devilskipper@gmail.com>
2023-11-30 21:36:05 +0700

    Update new

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -1,23 +1,5 @@
 package Menu;
-
-
-import HeThongGiaoDuc.ChuongTrinhHoc.ChuongTrinhHoc;
-import HeThongGiaoDuc.LopHoc.TrangThaiLop;
-import HeThongGiaoDuc.PhongVan.KetQuaPhongVan;
-import HeThongGiaoDuc.PhongVan.LichPhongVan;
-import HeThongGiaoDuc.PhongVan.LienHe;
-import HeThongGiaoDuc.PhongVan.TrangThaiPhongVan;
 import Menu.Actor.ActorKhachHang;
-import NguoiDung.User;
-import NguoiDung.VaiTro;
-import QuanLyDoiTuong.*;
-import Utils.ScannerUtils;
-
-import java.sql.SQLOutput;
-import java.time.LocalDate;
-import java.time.LocalTime;
-
-
 public class Menu {
     private static final ActorKhachHang actorKhachHang = new ActorKhachHang();
     public static void main(String[] args) {
