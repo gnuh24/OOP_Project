c7400b6b342a4f738840f2181cce477ac4bb0b32
Tran Khanh <khanhday1124@gmail.com>
2023-12-01 15:57:56 +0700

    actor quanly

--- src/Menu/Actor/ActorGiamDoc.java
+++ src/Menu/Actor/ActorGiamDoc.java
@@ -1,4 +1,15 @@
 package Menu.Actor;
 
+import QuanLyDoiTuong.QLChuongTrinhHoc;
+import Utils.ScannerUtils;
+
 public class ActorGiamDoc extends ActorQuanLy{
+    public void thayDoiHocPhi(){
+        QLChuongTrinhHoc.inChuongTrinhHoc(QLChuongTrinhHoc.getDsChuongTrinhHoc());
+        System.out.println("Nhập mã chương trình muốn thay đổi học phí");
+        String maChuongTrinh=ScannerUtils.inputString();
+        System.out.println("Nhập học phí mới mới");
+        int hocPhi=ScannerUtils.inputInt();
+        QLChuongTrinhHoc.timKiemTheoMa(maChuongTrinh).setHocPhi(hocPhi);
+    }
 }
