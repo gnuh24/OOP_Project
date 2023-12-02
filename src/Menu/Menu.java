c20699017906273c86612b2f4852e0656d6c5240
mttk2004 <mttk2004@hotmail.com>
2023-12-02 19:30:17 +0700

    test

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -6,6 +6,7 @@ public class Menu {
     private static final GiaoDienKhachHang actorKhachHang = new GiaoDienKhachHang();
 
     public static void main(String[] args) {
+    	System.out.println("hdfds");
         LoadDuLieu.loading();
         actorKhachHang.giaoDien();
     }
